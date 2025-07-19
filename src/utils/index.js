9ed7abe88e0d86f282aaefb6207e628e1099bfd8
mahmoud-abd-elnasser <mahmoudabdelnaser248@gmail.com>
2025-07-19 16:38:16 +0300

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,10 +6,10 @@ import hmv from "/assets/videos/hero.mp4";
 import smallmv from "/assets/videos/smallHero.mp4";
 import highlightFirstmv from "/assets/videos/highlight-first.mp4";
 import highlightSectmv from "/assets/videos/hightlight-third.mp4";
-// import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";
-// import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
-// import exploremv from "/assets/videos/explore.mp4";
-// import framemv from "/assets/videos/frame.mp4";
+import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";
+import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
+import exploremv from "/assets/videos/explore.mp4";
+import framemv from "/assets/videos/frame.mp4";
 
 import apple from "/assets/images/apple.svg";
 import search from "/assets/images/search.svg";
@@ -32,15 +32,14 @@ import frame from "/assets/images/frame.png";
 export const heroVideo = hmv;
 export const smallHeroVideo = smallmv;
 export const highlightSecondVideo = highlightSectmv;
-export const highlightThirdVideo =
-  "https://pacmlrbftolukawjqnfi.supabase.co/storage/v1/object/sign/apple/hightlight-sec.mp4?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNjhiNDg4YS1jYTQyLTQ0NDUtODkwZi00NTJmMzg4MDQ1ZjYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhcHBsZS9oaWdodGxpZ2h0LXNlYy5tcDQiLCJpYXQiOjE3NTI5MjcwMDYsImV4cCI6MTc4NDQ2MzAwNn0.5oIIxMHsvgHRagIQZIWr0_YL7nkiVy8S27sKVMxQRCg";
-// export const highlightThirdVideo = highlightThirdmv;
-// export const highlightFourthVideo = highlightFourthmv;
-export const highlightFourthVideo =
-  "https://pacmlrbftolukawjqnfi.supabase.co/storage/v1/object/sign/apple/hightlight-fourth.mp4?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNjhiNDg4YS1jYTQyLTQ0NDUtODkwZi00NTJmMzg4MDQ1ZjYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhcHBsZS9oaWdodGxpZ2h0LWZvdXJ0aC5tcDQiLCJpYXQiOjE3NTI5MjY3NjksImV4cCI6MTc4NDQ2Mjc2OX0.PdXBumkxsLWowM6amEVHOpBS2QKgMxL6T8vVAgcBNqo";
-export const exploreVideo =
-  "https://pacmlrbftolukawjqnfi.supabase.co/storage/v1/object/sign/apple/explore.mp4?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNjhiNDg4YS1jYTQyLTQ0NDUtODkwZi00NTJmMzg4MDQ1ZjYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhcHBsZS9leHBsb3JlLm1wNCIsImlhdCI6MTc1MjkyNTA2NSwiZXhwIjoxNzg0NDYxMDY1fQ.FbY8ShNDI2Zo7rbegbxjTqzzWuFSALSPiep0RcOflz0";
-export const frameVideo =
+// export const highlightThirdVideo =
+//   "https://pacmlrbftolukawjqnfi.supabase.co/storage/v1/object/sign/apple/hightlight-sec.mp4?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNjhiNDg4YS1jYTQyLTQ0NDUtODkwZi00NTJmMzg4MDQ1ZjYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhcHBsZS9oaWdodGxpZ2h0LXNlYy5tcDQiLCJpYXQiOjE3NTI5MjcwMDYsImV4cCI6MTc4NDQ2MzAwNn0.5oIIxMHsvgHRagIQZIWr0_YL7nkiVy8S27sKVMxQRCg";
+export const highlightThirdVideo = highlightThirdmv;
+export const highlightFourthVideo = highlightFourthmv;
+// export const highlightFourthVideo =
+//   "https://pacmlrbftolukawjqnfi.supabase.co/storage/v1/object/sign/apple/hightlight-fourth.mp4?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNjhiNDg4YS1jYTQyLTQ0NDUtODkwZi00NTJmMzg4MDQ1ZjYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhcHBsZS9oaWdodGxpZ2h0LWZvdXJ0aC5tcDQiLCJpYXQiOjE3NTI5MjY3NjksImV4cCI6MTc4NDQ2Mjc2OX0.PdXBumkxsLWowM6amEVHOpBS2QKgMxL6T8vVAgcBNqo";
+export const exploreVideo = exploremv;
+  export const frameVideo =
   "https://pacmlrbftolukawjqnfi.supabase.co/storage/v1/object/sign/apple/frame.mp4?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNjhiNDg4YS1jYTQyLTQ0NDUtODkwZi00NTJmMzg4MDQ1ZjYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhcHBsZS9mcmFtZS5tcDQiLCJpYXQiOjE3NTI5MjQ1MzYsImV4cCI6MTc4NDQ2MDUzNn0.Tk5AEuDxaVegvT_eGO5s1fikVceRC2_TFF7OmoWsEk4";
 
 export const appleImg = apple;
